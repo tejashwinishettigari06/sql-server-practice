e80e832a49d112843c4f1d74f271d00cc2d1ef6b
tejashwini <tejashwinishettigari06@gmail.com>
2026-07-01 22:43:27 +0530

    Message

--- 01_DQL/select.sql
+++ 01_DQL/select.sql
@@ -8,3 +8,7 @@ select first_name,country,score from customers
 
 --select using where clause- for filtering data based on a condition
 select score from customers where score!=0
+select first_name from customers where country='Germany'
+
+--select using order by: for sorting the data
+select * from customers order by score desc
