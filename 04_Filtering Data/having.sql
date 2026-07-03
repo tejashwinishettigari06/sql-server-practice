b3c0d528e448b3f01c3bd6893ebf4c79d3d58849
tejashwini <tejashwinishettigari06@gmail.com>
2026-07-03 21:28:53 +0530

    Message

---
+++ 04_Filtering Data/having.sql
@@ -0,0 +1,7 @@
+-- having clause : filters data after aggregation
+-- used only with group by
+
+/* find the average score for each country considering only customers
+with a score not equal to 0 and return only those countries with an average score greater than 430 */
+
+select country,avg(score) as average_score from customers where score !=0 group by country having avg(score) > 430
